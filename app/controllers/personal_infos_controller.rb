5e5eb7a5db79e3d0681c2c343413e8b473f08de3
Haruko Okada <hokada@uiowa.edu>
2023-09-23 23:06:14 -0500

    personal info api attempt

--- app/controllers/personal_infos_controller.rb
+++ app/controllers/personal_infos_controller.rb
@@ -1,5 +1,6 @@
 class PersonalInfosController < ApplicationController
 
+  @personal_info = PersonalInfo.last
   def new
     @personal_info = PersonalInfo.new
   end
@@ -9,8 +10,9 @@ class PersonalInfosController < ApplicationController
 
     if @personal_info.save
       redirect_to @personal_info, notice: 'Personal info was successfully created.'
+      render json: { success: true, personal_info: @personal_info.as_json }
     else
-      render :new
+      render json: { success: false, errors: @personal_info.errors.full_messages }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # get '', to: 'experiences#resume'
   root 'resumes#index'
-  resources :personal_informations, only: [:new, :create]
-  # get 'experiences', to: 'experiences#index'
-  # post 'experiences', to: 'experiences#add'
-  # delete 'experiences/:id', to: 'experiences#delete'
+  resources :personal_infos, only: [:new, :create]
+
+  namespace :api do
+    resources :personal_infos, only: [:index] # You can define the specific actions you need
+  end
+
+  get 'experiences', to: 'experiences#index'
+  post 'experiences', to: 'experiences#add'
+  delete 'experiences/:id', to: 'experiences#delete'
 end
