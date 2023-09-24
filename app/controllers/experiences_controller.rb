1694b53c062f2cc659a4f7bede600011adbceb34
Haruko Okada <hokada@uiowa.edu>
2023-09-24 11:56:39 -0500

    giving up for now...will come back later

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,10 +3,28 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
   end
 
+  def create
+    # Create a new experience record
+    @experience = Experience.new(exp_params)
+
+    if @experience.save
+
+      experience_data = @experience
+      title = experience_data["title"]
+      id = experience_data.id
+      puts "titleee and id"
+      puts title, id
+      description = experience_data.experience_descriptions.where(experience_id: id)
+      puts description
+
+    end
+  end
+
   def add
     exp = Experience.new(exp_params)
     if exp.save
-      render json: { status: 'success', experience: exp }, status: :created
+      # render json: { status: 'success', experience: exp }, status: :created
+      puts exp.to_json
     else
       render json: { status: 'error', errors: exp.errors.full_messages }, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
     resources :personal_infos, only: [:index] # You can define the specific actions you need
   end
 
+  resources :experiences, only: [:new, :create]
+
   get 'experiences', to: 'experiences#index'
-  post 'experiences', to: 'experiences#add'
+  post 'experiences', to: 'experiences#create'
   delete 'experiences/:id', to: 'experiences#delete'
 end
