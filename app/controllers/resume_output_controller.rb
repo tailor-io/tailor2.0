8ed9f28fd81fd34c8895f1486b51732ff3378b5a
Haruko Okada <hokada@uiowa.edu>
2023-09-24 12:41:13 -0500

    fetching scraper attempt

--- app/controllers/resume_output_controller.rb
+++ app/controllers/resume_output_controller.rb
@@ -7,11 +7,12 @@ class ResumeOutputController < ApplicationController
   end
 
   def submit_url
-    @resume =
-    url = params[:company_url]
+    @resume = Resume.new
+    HARDurl = "https://boards.greenhouse.io/adept/jobs/4265377006"
+
     user_id = "650f3a5e13ee8d6494714010"
-    if @personal_info.save
-      uri = URI("https://tailor-job-scraper-7e4b7f88930e.herokuapp.com/scrape?url=#{CGI.escape(url)}&user-id=650f3a5e13ee8d6494714010")
+    if @resume.save
+      uri = URI("https://tailor-job-scraper-7e4b7f88930e.herokuapp.com/scrape?url=#{CGI.escape(HARDurl)}&user-id=650f3a5e13ee8d6494714010")
 
       # Create a Net::HTTP object and configure it
       http = Net::HTTP.new(uri.host, uri.port)
@@ -34,6 +35,11 @@ class ResumeOutputController < ApplicationController
       render json: { success: false, errors: @personal_info.errors.full_messages }
     end
 
+  end
+
+  private
+  def resume_output_params
+    params.require(:resume_output).permit(:company_url)
   end
 end
 
